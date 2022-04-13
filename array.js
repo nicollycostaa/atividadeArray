e9ea7f613bdd0b1f2c0bdfa8ba5e8792989a844e
nicollycostaa <92127366+nicollycostaa@users.noreply.github.com>
2022-04-13 15:14:08 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,20 @@
+const alunos = [
+    {
+        nome: "Nicolly",
+        nota: 9.8   
+    },
+    {
+        nome: "Gabrela",
+        nota: 10
+    },
+    {
+        nome: "Ana",
+        nota:8.5
+    }
+]
+
+const media = (alunos[0].nota + alunos[1].nota+ alunos[2].nota) / 3;
+
+const nomesDeAlunos = ["Nicolly", "Diego", "Carla"];
+
+console.log(alunos);
